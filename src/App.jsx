9b01b3a1a0ace013f5bdf8cf8cbf512f6d573a39
nvhung2711 <nvhung2711@gmail.com>
2025-11-13 21:53:55 +1100

    updating the count on for trending movies

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import heroImg from './assets/hero-img.png';
 import Search from './components/Search';
 import Spinner from './components/Spinner';
 import MovieCard from './components/MovieCard';
-import { updateSearchCount } from './appwrite';
+import { getTrendingMovies, updateSearchCount } from './appwrite';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 
@@ -23,10 +23,11 @@ const App = () => {
     const [searchTerm, setSearchTerm] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
     const [movieList, setMovieList] = useState([]);
+    const [trendingMovies, setTrendingMovies] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
 
-    useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm]);
+    useDebounce(() => setDebouncedSearchTerm(searchTerm), 1000, [searchTerm]);
 
     const fetchMovies = async (query = '') => {
         setIsLoading(true);
@@ -60,9 +61,23 @@ const App = () => {
         }
     };
 
+    const loadTrendingMovies = async () => {
+        try {
+            const movies = await getTrendingMovies();
+
+            setTrendingMovies(movies);
+        } catch (error) {
+            console.error(`Error fetching trending movies: ${error}`);
+        }
+    };
+
     useEffect(() => {
         fetchMovies(debouncedSearchTerm);
     }, [debouncedSearchTerm]);
+
+    useEffect(() => {
+        loadTrendingMovies();
+    }, []);
 
     return (
         <main>
@@ -82,8 +97,26 @@ const App = () => {
                     />
                 </header>
 
+                {trendingMovies.length > 0 && (
+                    <section className="trending">
+                        <h2>Trending Movies</h2>
+
+                        <ul>
+                            {trendingMovies.map((movie, index) => (
+                                <li key={movie.$id}>
+                                    <p>{index + 1}</p>
+                                    <img
+                                        src={movie.poster_url}
+                                        alt={movie.title}
+                                    />
+                                </li>
+                            ))}
+                        </ul>
+                    </section>
+                )}
+
                 <section className="all-movies">
-                    <h2 className="mt-10">All Movies</h2>
+                    <h2>All Movies</h2>
                     {isLoading ? (
                         <Spinner />
                     ) : errorMessage ? (
